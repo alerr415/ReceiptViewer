08693bd8d549d1162e8b6a5caf186a653b78b5b4
alerr415 <alerr415@hotmail.com>
2022-07-06 20:46:54 +0200

    Initial Commit

---
+++ ReceiptViewer/ReceiptViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptViewerApp.swift
+//  ReceiptViewer
+//
+//  Created by Alejandro R. Rodriguez on 2022. 07. 06..
+//
+
+import SwiftUI
+
+@main
+struct ReceiptViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
